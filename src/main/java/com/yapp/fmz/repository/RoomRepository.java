d8f217f24e2c584abc4c82acf01c94829d349c03
wonheejeong <whj6648@gmail.com>
2020-04-29 11:50:39 +0900

    Revert "add 매물 정보 받아오기 & Swagger 연동"
    This reverts commit d448dc4a23f7602506526341f1f1c99d79bb1357.

--- src/main/java/com/yapp/fmz/repository/RoomRepository.java
+++ src/main/java/com/yapp/fmz/repository/RoomRepository.java
@@ -1,17 +1,7 @@
 package com.yapp.fmz.repository;
 
 import com.yapp.fmz.domain.Room;
-import com.yapp.fmz.domain.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-
-@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
-
-    @Query(value = "SELECT  r from Room r  Where r.zone.id = ?1")
-    public List<Room> findRoomsByZone(Long zone_id);
 }
