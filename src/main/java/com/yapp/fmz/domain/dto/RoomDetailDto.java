24be29be550f2ca3e6ed108c7274047e799a23a1
wonheejeong <whj6648@gmail.com>
2020-05-14 04:58:49 +0900

    매물 데이터 등록 번호 추가

--- src/main/java/com/yapp/fmz/domain/dto/RoomDetailDto.java
+++ src/main/java/com/yapp/fmz/domain/dto/RoomDetailDto.java
@@ -7,29 +7,31 @@ import lombok.Data;
 public class RoomDetailDto{
 
 
-        private Long id;
-        private String name;
-        private Long zipcode;
-        private String address;
-        private String roomType;
-        private String img;
-        private String buildingType;
-        private String loanType;
-        private Long deposit;
-        private Long monthlyPayment;
-        private Location location;
+    private Long id;
+    private String name;
+    private Long zipcode;
+    private String address;
+    private String roomType;
+    private String img;
+    private String buildingType;
+    private String loanType;
+    private Long deposit;
+    private Long registerId;
+    private Long monthlyPayment;
+    private Location location;
 
-        public RoomDetailDto(Room room) {
-            this.id = room.getId();
-            this.name = room.getName();
-            this.zipcode = room.getZipcode();
-            this.address = room.getAddress();
-            this.img = room.getImg();
-            this.buildingType = room.getBuildingType();
-            this.roomType = room.getRoomType();
-            this.loanType = room.getLoanType();
-            this.deposit = room.getDeposit();
-            this.monthlyPayment = room.getMonthlyPayment();
-            this.location = room.getLocation();
-        }
+    public RoomDetailDto(Room room) {
+        this.id = room.getId();
+        this.name = room.getName();
+        this.zipcode = room.getZipcode();
+        this.address = room.getAddress();
+        this.img = room.getImg();
+        this.buildingType = room.getBuildingType();
+        this.roomType = room.getRoomType();
+        this.loanType = room.getLoanType();
+        this.deposit = room.getDeposit();
+        this.registerId = room.getRegisterId();
+        this.monthlyPayment = room.getMonthlyPayment();
+        this.location = room.getLocation();
+    }
 }
